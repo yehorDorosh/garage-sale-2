ac41abc8a6704204e94224792bd8cecd9cae8f4c
Yehor Dorosh <dorosh.y@brainrocket.com>
2025-06-04 14:51:20 +0100

    Add firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import firebaseConfig from '../app/security/firebase-config';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
