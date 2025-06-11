e0624146df099d97e217885453465b9ef31e2863
Yehor Dorosh <dorosh.y@brainrocket.com>
2025-06-11 14:14:58 +0100

    Add reactive form

--- src/app/components/auth/signup-form/signup-form.ts
+++ src/app/components/auth/signup-form/signup-form.ts
@@ -1,9 +1,100 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup-form.html',
   styleUrl: './signup-form.scss',
 })
-export class SignupForm {}
+export class SignupForm {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(254),
+      ],
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(254),
+        Validators.pattern('^(?=.*[A-Z])(?=.*\\d).+$'),
+      ],
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [this.comparePasswords],
+    }),
+    phone: new FormControl('', {
+      validators: [
+        Validators.maxLength(254),
+        Validators.pattern('^\\+[0-9]{1,3}[0-9\\s\\-()]{6,19}$'),
+      ],
+    }),
+    messenger: new FormControl('', {
+      validators: [Validators.maxLength(254)],
+    }),
+    slackUsername: new FormControl('', {
+      validators: [Validators.maxLength(254)],
+    }),
+  });
+
+  comparePasswords(control: AbstractControl) {
+    if (!control.parent) {
+      return null;
+    }
+
+    const password = control.parent.get('password')?.value;
+    const confirmPassword = control.value;
+
+    if (password === confirmPassword) {
+      return null;
+    }
+
+    return { passwordsDoesntMatch: true };
+  }
+
+  get emailInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get passwordInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  get confirmPasswordInvalid() {
+    return (
+      this.form.controls.confirmPassword.touched &&
+      this.form.controls.confirmPassword.dirty &&
+      this.form.controls.confirmPassword.invalid
+    );
+  }
+
+  get phoneInvalid() {
+    return (
+      this.form.controls.phone.touched &&
+      this.form.controls.phone.dirty &&
+      this.form.controls.phone.invalid
+    );
+  }
+
+  onSubmit() {
+    console.log(this.form, this.form.value?.email);
+  }
+}
