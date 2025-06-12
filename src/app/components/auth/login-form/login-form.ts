e8ca02f1da895198018fb07223e6ded64c1b4da5
Yehor Dorosh <dorosh.y@brainrocket.com>
2025-06-12 12:50:29 +0100

    Add login form

---
+++ src/app/components/auth/login-form/login-form.ts
@@ -0,0 +1,71 @@
+import { Component, inject, signal } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FirebaseError } from 'firebase/app';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-form.html',
+  styleUrl: './login-form.scss',
+})
+export class LoginForm {
+  private auth = inject(Auth);
+
+  loading = signal(false);
+  beError = '';
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  });
+
+  get emailInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get passwordInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  async onSubmit() {
+    this.loading.set(true);
+    this.beError = '';
+    try {
+      const { email, password } = this.form.value;
+      if (email && password) {
+        await signInWithEmailAndPassword(this.auth, email, password);
+        console.log('Login successful');
+      } else {
+        throw new Error('Email and password are required');
+      }
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        this.beError = error.code;
+      } else if (error instanceof Error) {
+        this.beError = error.message;
+      } else {
+        this.beError = 'An unknown error occurred';
+      }
+    } finally {
+      this.loading.set(false);
+    }
+  }
+}
--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MainTemp } from "../../components/layout/page-template/main-temp/main-temp";
+import { MainTemp } from '../../components/layout/page-template/main-temp/main-temp';
+import { LoginForm } from '../../components/auth/login-form/login-form';
 
 @Component({
   selector: 'app-login-page',
-  imports: [MainTemp],
+  imports: [MainTemp, LoginForm],
   templateUrl: './login-page.html',
-  styleUrl: './login-page.scss'
+  styleUrl: './login-page.scss',
 })
-export class LoginPage {
-
-}
+export class LoginPage {}
