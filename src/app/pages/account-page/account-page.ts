23cd8bc29522cc72f14373afc29105f82185f706
Yehor Dorosh <dorosh.y@brainrocket.com>
2025-06-12 12:32:07 +0100

    Add delete accout func

--- src/app/pages/account-page/account-page.ts
+++ src/app/pages/account-page/account-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MainTemp } from '../../components/layout/page-template/main-temp/main-temp';
+import { Auth, deleteUser } from '@angular/fire/auth';
+import { Firestore, doc, deleteDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-account-page',
@@ -7,4 +9,32 @@ import { MainTemp } from '../../components/layout/page-template/main-temp/main-t
   templateUrl: './account-page.html',
   styleUrl: './account-page.scss',
 })
-export class AccountPage {}
+export class AccountPage {
+  auth = inject(Auth);
+  firestore = inject(Firestore);
+
+  async onDeleteAccount() {
+    if (
+      window.confirm(
+        'Are you sure you want to delete your account? This action cannot be undone.'
+      )
+    ) {
+      const user = this.auth.currentUser;
+      if (user) {
+        try {
+          await deleteDoc(doc(this.firestore, 'users', user.uid));
+          await deleteUser(user);
+          console.log('Account deleted successfully.');
+        } catch (error) {
+          console.log('Failed to delete account: ' + error);
+          window.alert(
+            'Failed to delete account: ' +
+              (error instanceof Error ? error.message : error)
+          );
+        }
+      } else {
+        console.log('No user is currently signed in.');
+      }
+    }
+  }
+}
