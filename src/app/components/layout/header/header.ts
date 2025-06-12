0733ad39cdd01f7823bef578a58fecc3f5b14e71
Yehor Dorosh <dorosh.y@brainrocket.com>
2025-06-12 14:28:41 +0100

    Add logout

--- src/app/components/layout/header/header.ts
+++ src/app/components/layout/header/header.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth, signOut } from '@angular/fire/auth';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [RouterModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
+  auth = inject(Auth);
 
+  onLogout() {
+    signOut(this.auth)
+      .then(() => {
+        console.log('Logout successful');
+      })
+      .catch((error) => {
+        console.error('Logout failed', error);
+      });
+  }
 }
